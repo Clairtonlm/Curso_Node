e7f2db4b973fa6db5ae79c28e7f10a1a96c305f4
Clairtonlm <clairtonlm@gmail.com>
2023-07-20 23:46:51 -0300

    Crinado endPoint

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", function(req, res){
+    res.send("Hello word curso Node")
+});
+
+app.delete("/user", function(req, res){
+    res.send({
+        nome: "JOse deletado"
+        
+    })
+});
+
+app.listen(8080, function(){
+    console.log("Servidor Funcionando")
+})
