7273958ad11a6acc54b6d3776be1b5e816061e56
Clairtonlm <clairtonlm@gmail.com>
2023-07-27 00:29:06 -0300

    Trabalhando com paramentros

--- 01-ServidorExpress/index.js
+++ 01-ServidorExpress/index.js
@@ -6,6 +6,22 @@ const app = express();
 app.get("/", function(req, res){
     res.send("<h1>Bem vindo ao curso de Node</h1>")
 });
+//parametro obrigatorio
+app.get("/ola/:nome", function(req, res){
+    var nome = req.params.nome;
+    res.send("<h1>Oi " + nome + "</h1>");
+});
+
+//parametro não obrigatorio
+app.get("/blog/:artigo?", function(req, res){
+    var artigo = req.params.artigo;
+    res.send("<h1>nome " + artigo + "</h1>");
+    if(artigo){
+        res.send("<h2>Artigo" + artigo + "</h2>")
+    }else{
+        res.send("Usuário  nao passou artigo")
+    }
+});
 
 
 
