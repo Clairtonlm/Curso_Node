397d9dab7f8589cc0aa2a39447761451199dc335
Clairtonlm <clairtonlm@gmail.com>
2023-07-31 23:30:20 -0300

    Crinado servidor Express

---
+++ 06-LivrariaBook/src/app.js
@@ -0,0 +1,9 @@
+import { Express } from "express";
+const app = Express();
+
+const livros = [
+    {id: 1, "titulo":"O senhor dos Aneis"},
+    {id: 2, "titulo":"Curso de Node"},
+    {id: 3, "titulo":"A volta dos que nao foram"},
+
+]
